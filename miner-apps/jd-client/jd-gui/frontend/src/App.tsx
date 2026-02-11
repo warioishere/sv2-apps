8f4bc8332d575a2bf5c740d0842137f0d9aeb2a1
wario_is_here <mario.hofmann@yourdevice.ch>
2026-02-11 13:26:31 +0100

    Update version to 0.1.0

--- miner-apps/jd-client/jd-gui/frontend/src/App.tsx
+++ miner-apps/jd-client/jd-gui/frontend/src/App.tsx
@@ -68,7 +68,7 @@ function App() {
       </main>
 
       <footer className="app-footer">
-        <p>JD-Client Web GUI Manager v1.0.0</p>
+        <p>JD-Client Web GUI Manager v0.1.0</p>
       </footer>
     </div>
   );
